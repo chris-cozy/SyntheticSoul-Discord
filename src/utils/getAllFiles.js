2883191cfdbf67e06c2904b2b22f61748487a849
chris-cozy <csande9@clemson.edu>
2023-04-09 19:30:55 -0400

    added module to get all files

---
+++ src/utils/getAllFiles.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+/*
+    This grabs all of the files/folders in a directory
+*/
+module.exports = (directory, foldersOnly = false) => {
+    let fileNames = [];
+
+    const files = fs.readdirSync(directory, { withFileTypes: true });
+
+    for (const file of files) {
+        const filePath = path.join(directory, file.name);
+
+        if (foldersOnly) {
+            if (file.isDirectory()) {
+                fileNames.push(filePath)
+            }
+        } else {
+            if (file.isFile()) {
+                fileNames.push(filePath);
+            }
+        }
+    }
+
+    return fileNames;
+}
