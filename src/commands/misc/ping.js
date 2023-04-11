3dbc8720b177a0742a3f768fba6a86466d9ea94d
chris-cozy <csande9@clemson.edu>
2023-04-10 21:10:24 -0400

    updated ping command to show client ping

--- src/commands/misc/ping.js
+++ src/commands/misc/ping.js
@@ -3,11 +3,16 @@
  */
 module.exports = {
     name: 'ping',
-    description: 'ping pong - client websocket ping',
+    description: 'ping pong - client and websocket ping',
     devonly: true,
     testOnly: true,
 
-    callback: (client, interaction) => {
-        interaction.reply(`pong: ${client.ws.ping}ms`);
+    callback: async (client, interaction) => {
+        await interaction.deferReply();
+
+        const reply = await interaction.fetchReply();
+
+        const ping = reply.createdTimestamp - interaction.createdTimestamp;
+        interaction.editReply(`pong! my personal ping is ${ping}ms, and the websocket ping is ${client.ws.ping}ms`);
     },
 }
