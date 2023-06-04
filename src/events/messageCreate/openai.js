c25557871da8b25642064729686c18bd61cee7a7
chris-cozy <csande9@clemson.edu>
2023-06-04 12:10:54 -0400

    Progress on filtering previous messages. Unfinished

--- src/events/messageCreate/openai.js
+++ src/events/messageCreate/openai.js
@@ -6,9 +6,11 @@ const chatCost = 25;
 /**
  * @brief Handle a message sent in the server, using the openai gpt-3.5 API
  * @param {Client} client - The bot
- * @param {Message} message - The message which was sent
+ * @param {Message} discordQuery - The message which was sent
  */
-module.exports = async (client, message) => {
+module.exports = async (client, discordQuery) => {
+
+    const persona = `Talk like you are a young woman with a cute personality. My name is ${discordQuery.author.username}.`
 
     // Setup openai connection
     const configuration = new Configuration({
@@ -18,26 +20,19 @@ module.exports = async (client, message) => {
 
 
     // Ignore msg if author is a bot
-    if (message.author.bot) {
+    if (discordQuery.author.bot) {
         return;
     }
 
-    // Ignore msg if not sent in the right channel
-    /*
-    if (message.channel.id !== process.env.OPENAI_CHANNEL_ID) {
-        console.log('not the designated channel');
-    }
-    */
-
     // Ignore message if bot is not mentioned
-    if (!message.mentions.has(client.user.id)) {
+    if (!discordQuery.mentions.has(client.user.id)) {
         return;
     }
 
     // Update user balance
     let query = {
-        userId: message.author.id,
-        guildId: message.guild.id,
+        userId: discordQuery.author.id,
+        guildId: discordQuery.guild.id,
     };
 
     let user = await userBalance.findOne(query);
@@ -49,51 +44,39 @@ module.exports = async (client, message) => {
                 user.balance -= chatCost;
                 await user.save();
             } else {
-                message.reply("you don't have enough credits to chat, collect your dailys to gain more..");
+                discordQuery.reply("you don't have enough credits to chat, collect your dailys to gain more..");
                 return;
             }
         }
     } else {
-        message.reply("you don't currently have a balance yet to chat. run '/daily' to set one up..");
+        discordQuery.reply("you don't currently have a balance yet to chat. run '/daily' to set one up..");
         return;
     }
 
 
-    let conversationLog = []
-
-    // Give the language model instructions
-    conversationLog.push({
-        role: 'system',
-        content: `Your name is Jasmine but you like to be referred to as Jas, and you are a young woman with a cute personality. Talk like you are a young woman with a cute personality. My name is ${message.author.username}.`
-    });
-
-    // Give the language model instructions
-    conversationLog.push({
-        role: 'user',
-        content: `Your name is Jasmine but you like to be referred to as Jas, and you are a young woman with a cute personality. Talk like you are a young woman with a cute personality. My name is ${message.author.username}.`
-    });
+    let conversationLog = [
+        {
+            role: 'system',
+            content: persona
+        }
+    ]
 
     // Send the bot typing status
-    await message.channel.sendTyping();
+    await discordQuery.channel.sendTyping();
 
     // Grab previous messages in channel
     const msgLimit = 15;
-    let prevMessages = await message.channel.messages.fetch({ limit: msgLimit });
+    let prevMessages = await discordQuery.channel.messages.fetch({ limit: msgLimit });
     // Messages are in latest-oldest order, so flip
     prevMessages.reverse();
 
     // Loop through prev msgs to find conversation context between the msg sender and the bot
-    prevMessages.forEach((msg) => {
+    prevMessages.forEach(async (msg, index) => {
         // Pattern for removing mentions - currently unused
         let mention = /<@(.*?)>/;
 
-        // Ignore messages from other bots
-        if ((msg.author.id !== client.user.id) && message.author.bot) {
-            return;
-        }
-
         // Ensure that the messages being added are from the original message sender, or the bot
-        if ((msg.author.id !== message.author.id) && (msg.author.id !== client.user.id)) {
+        if ((msg.author.id !== discordQuery.author.id) && (msg.author.id !== client.user.id)) {
             return;
         }
 
@@ -101,50 +84,54 @@ module.exports = async (client, message) => {
         if ((msg.mentions.has(client.user.id)) || (msg.author.id == client.user.id)) {
 
             // Add messages to conversation log, with appropriate role
-            if (msg.author.id == message.author.id) {
+            if (msg.author.id == discordQuery.author.id) {
                 conversationLog.push({
                     role: 'user',
                     content: msg.content,
                 });
             } else if (msg.author.id == client.user.id) {
 
+                /*
                 conversationLog.push({
                     role: 'assistant',
                     content: msg.content,
                 });
+                */
 
                 /*
-                * Grab the message that msg is a reply to, and check if the
-                * author of that message is the same author of the original message
+                * If message is from bot, grab the message that it's a reply to, and check if the
+                * author of that message is the same author of the original message sender
                 * This makes sure that the bot is not adding their replies to other users
                 * to the conversation log with this user
                 */
 
-                /*
-                // Check if message is a reply to another
+                // Check if message is a reply
                 if (msg.reference) {
-                    
-                    refMsg = await msg.fetchReference();
-                    if (refMsg.author.id == message.author.id) {
-                        //console.log(msg);
-                        conversationLog.push({
-                            role: 'assistant',
-                            content: msg.content,
-                        });
+                    // Use message.reference.messageId to get the original message ID
+                    const originalMessage = await msg.channel.messages.fetch(msg.reference.messageId);
+
+                    // Check if the original message ID exists and is in the cache
+                    if (originalMessage) {
+                        // The current message is a reply to the original message
+                        if (originalMessage.author.id == discordQuery.author.id) {
+                            conversationLog.push({
+                                role: 'assistant',
+                                content: msg.content,
+                            });
+                        }
                     }
-                */
+                }
             }
         } else {
             return;
         }
     });
 
-    //console.log(conversationLog);
     const result = await openai.createChatCompletion({
         model: 'gpt-3.5-turbo',
         messages: conversationLog,
     })
 
-    message.reply(result.data.choices[0].message);
+    discordQuery.reply(result.data.choices[0].message);
     console.log(conversationLog);
 };
