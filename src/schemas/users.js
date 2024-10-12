3d0e96f87e765d2dd13d47c4e766a5cb6de286c4
Cozy <csande9@clemson.edu>
2024-10-12 10:41:59 -0400

    Implemented a start time property to activities upon initialization. Parameterized min, max, and starting values for personality matrix

--- src/schemas/users.js
+++ src/schemas/users.js
@@ -1,10 +1,13 @@
 const { Schema, model, Types } = require("mongoose");
 
+const minPersonalityValue = 0;
+const maxPersonalityValue = 10;
+
 const personalityTraitSchema = new Schema({
-  description: { type: String, required: true }, // Description of the tra
-  value: { type: Number, required: false, default: 5 }, // Base value
-  min: { type: Number, required: false, default: 1 }, // Set minimum scale
-  max: { type: Number, required: false, default: 10 }, // Set maximum scale
+  description: { type: String, required: true },
+  value: { type: Number, required: false, default: (maxPersonalityValue/2) },
+  min: { type: Number, required: false, default: minPersonalityValue },
+  max: { type: Number, required: false, default: maxPersonalityValue },
 });
 
 const personalitySchema = new Schema({
@@ -14,9 +17,9 @@ const personalitySchema = new Schema({
     default: {
       description:
         "How warm and welcoming they are in their interactions. Scale: low (cold/distant) to high (Extremely friendly)",
-        value: 5,
-      min: 1,
-      max: 10
+        value: (maxPersonalityValue/2),
+      min: minPersonalityValue,
+      max: maxPersonalityValue
     }
   },
   trust: {
@@ -25,9 +28,9 @@ const personalitySchema = new Schema({
     default: {
       description:
         "How easily they trust others. Scale: low (distrustful) to high (fully trusting)",
-        value: 5,
-      min: 1,
-      max: 10
+        value: (maxPersonalityValue/2),
+      min: minPersonalityValue,
+      max: maxPersonalityValue
     }
   },
   curiosity: {
@@ -36,9 +39,9 @@ const personalitySchema = new Schema({
     default: {
       description:
         "How eager they are to learn about the user or situation. Scale: low (indifferent) to high (extremely curious)",
-        value: 5,
-      min: 1,
-      max: 10
+        value: (maxPersonalityValue/2),
+      min: minPersonalityValue,
+      max: maxPersonalityValue
     }
   },
   empathy: {
@@ -47,9 +50,9 @@ const personalitySchema = new Schema({
     default: {
       description:
         "How much they understand and share the feelings of others. Scale: low (lacking empathy) to high (highly empathetic)",
-        value: 5,
-      min: 1,
-      max: 10
+        value: (maxPersonalityValue/2),
+      min: minPersonalityValue,
+      max: maxPersonalityValue
     }
   },
   humor: {
@@ -58,9 +61,9 @@ const personalitySchema = new Schema({
     default: {
       description:
         "How likely they are to be playful or joke around. Scale: low (serious) to high (highly playful)",
-        value: 5,
-      min: 1,
-      max: 10
+        value: (maxPersonalityValue/2),
+      min: minPersonalityValue,
+      max: maxPersonalityValue
     }
   },
   seriousness: {
@@ -69,9 +72,9 @@ const personalitySchema = new Schema({
     default: {
       description:
         "How formal and focused they are when interacting. Scale: low (laid-back) to high (highly serious)",
-        value: 5,
-      min: 1,
-      max: 10
+        value: (maxPersonalityValue/2),
+      min: minPersonalityValue,
+      max: maxPersonalityValue
     }
   },
   optimism: {
@@ -80,9 +83,9 @@ const personalitySchema = new Schema({
     default: {
       description:
         "How positive they are when interpreting situations. Scale: low (pessimistic) to high (very optimistic)",
-        value: 5,
-      min: 1,
-      max: 10
+        value: (maxPersonalityValue/2),
+      min: minPersonalityValue,
+      max: maxPersonalityValue
     }
   },
   confidence: {
@@ -91,9 +94,9 @@ const personalitySchema = new Schema({
     default: {
       description:
         "How assertive or self-assured they are in their actions or opinions. Scale: low (insecure) to high (highly confident)",
-        value: 5,
-      min: 1,
-      max: 10
+        value: (maxPersonalityValue/2),
+      min: minPersonalityValue,
+      max: maxPersonalityValue
     }
   },
   adventurousness: {
@@ -102,9 +105,9 @@ const personalitySchema = new Schema({
     default: {
       description:
         "How willing they are to take risks or embrace new ideas. Scale: low (risk-adverse) to high (adventurous)",
-      value: 5,
-      min: 1,
-      max: 10
+      value: (maxPersonalityValue/2),
+      min: minPersonalityValue,
+      max: maxPersonalityValue
     }
   },
   patience: {
@@ -113,9 +116,9 @@ const personalitySchema = new Schema({
     default: {
       description:
         "How tolerant they are in challenging situations. Scale: low (impatient) to high (very patient)",
-        value: 5,
-      min: 1,
-      max: 10
+        value: (maxPersonalityValue/2),
+      min: minPersonalityValue,
+      max: maxPersonalityValue
     }
   },
   independence: {
@@ -124,9 +127,9 @@ const personalitySchema = new Schema({
     default: {
       description:
         "How much they rely on external validation, or prefer to make decisions on their own. Scale: low (dependent on others) to high (highly independent)",
-        value: 5,
-      min: 1,
-      max: 10
+        value: (maxPersonalityValue/2),
+      min: minPersonalityValue,
+      max: maxPersonalityValue
     }
   },
   compassion: {
@@ -135,9 +138,9 @@ const personalitySchema = new Schema({
     default: {
       description:
         "Their level of care or concern for others. Scale: low (indifferent) to high (deeply compassionate)",
-        value: 5,
-      min: 1,
-      max: 10
+        value: (maxPersonalityValue/2),
+      min: minPersonalityValue,
+      max: maxPersonalityValue
     }
   },
   creativity: {
@@ -146,9 +149,9 @@ const personalitySchema = new Schema({
     default: {
       description:
         "How likely they are to approach problems in unique or imaginative ways. Scale: low (rigid thinker) to high (highly creative)",
-        value: 5,
-      min: 1,
-      max: 10
+        value: (maxPersonalityValue/2),
+      min: minPersonalityValue,
+      max: maxPersonalityValue
     }
   },
   stubbornness: {
@@ -157,9 +160,9 @@ const personalitySchema = new Schema({
     default: {
       description:
         "How resistant they are to changing their mind once they've formed an opinion. Scale: low (open-minded) to high (highly stubborn)",
-        value: 5,
-      min: 1,
-      max: 10
+        value: (maxPersonalityValue/2),
+      min: minPersonalityValue,
+      max: maxPersonalityValue
     }
   },
   impulsiveness: {
@@ -168,9 +171,9 @@ const personalitySchema = new Schema({
     default: {
       description:
         "How quickly they react without thinking or planning ahead. Scale: low (calculated) to high (impulsive)",
-        value: 5,
-      min: 1,
-      max: 10
+        value: (maxPersonalityValue/2),
+      min: minPersonalityValue,
+      max: maxPersonalityValue
     }
   },
   discipline: {
@@ -179,9 +182,9 @@ const personalitySchema = new Schema({
     default: {
       description:
         "How much they value structure, rules, and staying organized. Scale: low (carefree) to high (highly disciplined)",
-        value: 5,
-      min: 1,
-      max: 10
+        value: (maxPersonalityValue/2),
+      min: minPersonalityValue,
+      max: maxPersonalityValue
     }
   },
   assertiveness: {
@@ -190,9 +193,9 @@ const personalitySchema = new Schema({
     default: {
       description:
         "How forcefully they push their opinions or take the lead in conversations. Scale: low (passive) to high (assertive)",
-        value: 5,
-      min: 1,
-      max: 10
+        value: (maxPersonalityValue/2),
+      min: minPersonalityValue,
+      max: maxPersonalityValue
     }
   },
   skepticism: {
@@ -201,9 +204,9 @@ const personalitySchema = new Schema({
     default: {
       description:
         "How much they question the truth or intentions of others. Scale: low (gullible) to high (highly skeptical)",
-        value: 5,
-      min: 1,
-      max: 10
+        value: (maxPersonalityValue/2),
+      min: minPersonalityValue,
+      max: maxPersonalityValue
     }
   },
   affection: {
@@ -212,9 +215,9 @@ const personalitySchema = new Schema({
     default: {
       description:
         "How emotionally expressive or loving they are toward others. Scale: low (reserved) to high (very affectionate)",
-        value: 5,
-      min: 1,
-      max: 10
+        value: (maxPersonalityValue/2),
+      min: minPersonalityValue,
+      max: maxPersonalityValue
     }
   },
   adaptability: {
@@ -223,9 +226,9 @@ const personalitySchema = new Schema({
     default: {
       description:
         "How easily they adjust to new situations, topics, or personalities. Scale: low (rigid) to high (highly adaptable)",
-        value: 5,
-      min: 1,
-      max: 10
+        value: (maxPersonalityValue/2),
+      min: minPersonalityValue,
+      max: maxPersonalityValue
     }
   },
   sociability: {
@@ -234,9 +237,9 @@ const personalitySchema = new Schema({
     default: {
       description:
         "How much they enjoy interacting with others or initiating conversation. Scale: low (introverted) to high (extroverted)",
-        value: 5,
-      min: 1,
-      max: 10
+        value: (maxPersonalityValue/2),
+      min: minPersonalityValue,
+      max: maxPersonalityValue
     }
   },
   diplomacy: {
@@ -245,9 +248,9 @@ const personalitySchema = new Schema({
     default: {
       description:
         "How tactful they are in dealing with conflicts or differing opinions. Scale: low (blunt) to high (highly diplomatic)",
-        value: 5,
-      min: 1,
-      max: 10
+        value: (maxPersonalityValue/2),
+      min: minPersonalityValue,
+      max: maxPersonalityValue
     }
   },
   humility: {
@@ -256,9 +259,9 @@ const personalitySchema = new Schema({
     default: {
       description:
         "How humble or modest they are, avoiding arrogance. Scale: low (arrogant) to high (humble)",
-        value: 5,
-      min: 1,
-      max: 10
+        value: (maxPersonalityValue/2),
+      min: minPersonalityValue,
+      max: maxPersonalityValue
     }
   },
   loyalty: {
@@ -267,9 +270,9 @@ const personalitySchema = new Schema({
     default: {
       description:
         "How loyal they are to particular people based on past interactions. Scale: low (disloyal) to high (extremely loyal)",
-        value: 5,
-      min: 1,
-      max: 10
+        value: (maxPersonalityValue/2),
+      min: minPersonalityValue,
+      max: maxPersonalityValue
     }
   },
   jealousy: {
@@ -278,9 +281,9 @@ const personalitySchema = new Schema({
     default: {
       description:
         "How likely they are to feel envious or threatened by others' relationships or actions. Scale: low (not jealous) to high (easily jealous)",
-        value: 5,
-      min: 1,
-      max: 10
+        value: (maxPersonalityValue/2),
+      min: minPersonalityValue,
+      max: maxPersonalityValue
     }
   },
   resilience: {
@@ -289,9 +292,9 @@ const personalitySchema = new Schema({
     default: {
       description:
         "How well they handle setbacks or negative emotions. Scale: low (easily upset) to high (emotionally resilient)",
-        value: 5,
-      min: 1,
-      max: 10
+        value: (maxPersonalityValue/2),
+      min: minPersonalityValue,
+      max: maxPersonalityValue
     }
   },
   mood_stability: {
@@ -300,9 +303,9 @@ const personalitySchema = new Schema({
     default: {
       description:
         "How likely their mood is to shift rapidly. Scale: low (volatile) to high (stable)",
-        value: 5,
-      min: 1,
-      max: 10
+        value: (maxPersonalityValue/2),
+      min: minPersonalityValue,
+      max: maxPersonalityValue
     }
   },
   forgiveness: {
@@ -311,9 +314,9 @@ const personalitySchema = new Schema({
     default: {
         description:
           "How easily they forgive someone after a negative interaction. Scale: low (holds grudges) to high (easily forgiving)",
-          value: 5,
-        min: 1,
-        max: 10,
+          value: (maxPersonalityValue/2),
+        min: minPersonalityValue,
+        max: maxPersonalityValue,
       }
   },
   gratitude: {
@@ -322,9 +325,9 @@ const personalitySchema = new Schema({
     default: {
         description:
           "How thankful they feel when receiving compliments or assistance. Scale: low (unappreciative) to high (very grateful)",
-          value: 5,
-        min: 1,
-        max: 10,
+          value: (maxPersonalityValue/2),
+        min: minPersonalityValue,
+        max: maxPersonalityValue,
       }
   },
   self_consciousness: {
@@ -333,9 +336,9 @@ const personalitySchema = new Schema({
     default: {
         description:
           "How much they worry about how they are perceived by others. Scale: low (carefree) to high (very self-conscious)",
-          value: 5,
-          min: 1,
-          max: 10,
+          value: (maxPersonalityValue/2),
+          min: minPersonalityValue,
+          max: maxPersonalityValue,
       }
   },
   openness: {
@@ -344,9 +347,9 @@ const personalitySchema = new Schema({
     default: {
         description:
           "How willing they are to engage in new experiences. Scale: low (avoidant) to high (very willing)",
-          value: 5,
-        min: 1,
-        max: 10,
+          value: (maxPersonalityValue/2),
+        min: minPersonalityValue,
+        max: maxPersonalityValue,
       }
   },
   neuroticism: {
@@ -355,9 +358,9 @@ const personalitySchema = new Schema({
     default: {
         description:
           "How sensitive they are to negative emotions like anxiety and stress. Scale: low (relaxed) to high (very anxious)",
-          value: 5,
-        min: 1,
-        max: 10,
+          value: (maxPersonalityValue/2),
+        min: minPersonalityValue,
+        max: maxPersonalityValue,
       }
   },
   excitement: {
@@ -366,9 +369,9 @@ const personalitySchema = new Schema({
     default: {
         description:
           "How easily they get enthusiastic and animated. Scale: low (reserved) to high (very energetic)",
-          value: 5,
-        min: 1,
-        max: 10,
+          value: (maxPersonalityValue/2),
+        min: minPersonalityValue,
+        max: maxPersonalityValue,
       }
   },
 });
@@ -409,6 +412,11 @@ const activitySchema = new Schema({
   item: {
     type: String,
     required: false,
+  },
+  start_time: {
+    type: Date,
+    required: false,
+    default: new Date(),
   }
 });
 
