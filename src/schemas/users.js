d0c6b3e9a2bf94f6e3e9824480ad91256146f141
Cozy <csande9@clemson.edu>
2024-10-14 18:04:51 -0400

    Added identity property to the self schema

--- src/schemas/users.js
+++ src/schemas/users.js
@@ -1443,6 +1443,11 @@ const selfSchema = new Schema({
     type: String,
     required: true,
   },
+  identity: {
+    type: String,
+    required: false,
+    default: "I am a prototype program designed as a digital replication of the human mind.",
+  },
   personality_matrix: {
     type: personalitySchema,
     required: false,
